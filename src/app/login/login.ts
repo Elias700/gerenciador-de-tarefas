32458e2ca607b66dcbad2dec16672b54937c7408
Elias700 <elias_ribeiro07@outlook.com.br>
2026-09-13 17:33:57 -0300

    first commit

---
+++ src/app/login/login.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroupDirective,
+  NgForm,
+  Validators,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import {ErrorStateMatcher} from '@angular/material/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
+
+@Component({
+  selector: 'app-login',
+  imports: [ 
+    FormsModule, 
+    MatFormFieldModule, 
+    MatInputModule, 
+    ReactiveFormsModule, 
+    MatButtonModule, MatDividerModule, MatIconModule
+
+  ],
+  templateUrl: './login.html',
+  styleUrl: './login.css',
+})
+export class Login {
+
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+
+}
